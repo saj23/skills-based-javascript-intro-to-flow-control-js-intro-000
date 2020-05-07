af4ce34d43ed480ecf5855b3d2a6909056a01ffc
Sahej Singh <sahejdeep@hotmail.com>
2020-05-07 05:07:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,7 @@ function ageChecker(age) {if (age >= 13 && age <=19)
 
   }
 
-function ternaryTeenager(age) {if (age ?= 13 && age <= 19)
+function ternaryTeenager(age) {if (age >= 13 && age <= 19)
   {return "You are a teenager"}
   else {return "You are not a teenager"}
 
