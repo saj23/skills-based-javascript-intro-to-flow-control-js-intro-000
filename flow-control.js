de33dab1dd25ea7ec9461554f30585661648147f
Sahej Singh <sahejdeep@hotmail.com>
2020-05-07 05:15:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,6 +21,15 @@ function ternaryTeenager(age) {if (age >= 13 && age <= 19)
 
 }
 
-function switchAge(age) {
+function switchAge(age) {switch (age)
+  {case 13: return "You are a teenager"
+case 14: return "You are a teenager"
+case 15: return "You are a teenager"
+case 16: return "You are a teenager"
+case 17: return "You are a teenager"
+case 18: return "You are a teenager"
+case 18: return "You are a teenager"
+default: return "You have an age"
+}
 
 }
