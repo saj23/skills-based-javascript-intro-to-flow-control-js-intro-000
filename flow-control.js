2059cbd198f2da9b30069afcfbe9f0a93318e51b
Sahej Singh <sahejdeep@hotmail.com>
2020-05-07 05:04:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,7 +12,7 @@ function ageChecker(age) {if (age >= 13 && age <=19)
   {return "You are a teenager!"}
   else if (age <=12) {return "You are a kid"}
   else {return "You are a grownup"}
-    
+
   }
     
 
