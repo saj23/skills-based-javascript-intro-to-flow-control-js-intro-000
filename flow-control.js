b94a8907e503eda9546938188d15d509579bdd30
Sahej Singh <sahejdeep@hotmail.com>
2020-05-07 04:58:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,8 @@ function basicTeenager(age) {if(age >=13 && age <=19)
   {return "You are a teenager!";}
 }
 
-function teenager(age) {
+function teenager(age) {if(age >=13 && age <=19)
+  {return "You are a teenager!";}
 
 }
 
